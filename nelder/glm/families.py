efcc4251522a6ffb4daf14feb7e516c0a9b59cba
rjt1990 <rj-taylor@live.co.uk>
2016-08-21 09:30:35 +0100

    small update

--- nelder/glm/families.py
+++ nelder/glm/families.py
@@ -6,112 +6,112 @@ from .. import model as model
 
 class Normal(object):
 
-	def __init__(self):
-		"""
-		Contains information on whether the family has certain kinds of latent variables - e.g. a dispersion (scale) latent variable -
-		as well as information on how many additional distributional variables are required for this distribution
-		"""
-		self.scale = True
-		self.dof = False
-		self.additional_variables = 1
+    def __init__(self):
+        """
+        Contains information on whether the family has certain kinds of latent variables - e.g. a dispersion (scale) latent variable -
+        as well as information on how many additional distributional variables are required for this distribution
+        """
+        self.scale = True
+        self.dof = False
+        self.additional_variables = 1
 
-	@staticmethod
-	def neg_loglikelihood(Y, theta, scale, dof):
-		"""
-		Creates the negative loglikelihood for the model
+    @staticmethod
+    def neg_loglikelihood(Y, theta, scale, dof):
+        """
+        Creates the negative loglikelihood for the model
 
-		Parameters
+        Parameters
         -------
         Y - tf.placeholder
-        	The data for the target variable
+            The data for the target variable
 
         theta - tf.placeholder
-        	the location parameter for the distribution
+            the location parameter for the distribution
 
         scale - tf.placeholder
-        	the dispersion parameter for the distribution
+            the dispersion parameter for the distribution
 
         dof - tf.placeholder
-        	the degrees of freedom parameter for the distribution
+            the degrees of freedom parameter for the distribution
 
-		Returns
+        Returns
         -------
         - An tf.placeholder with the negative loglikelihood 
-		"""
-		return -tf.contrib.distributions.Normal(theta, scale).log_likelihood(Y)
+        """
+        return -tf.contrib.distributions.Normal(theta, scale).log_likelihood(Y)
 
 class Poisson(object):
 
-	def __init__(self):
-		"""
-		Contains information on whether the family has certain kinds of latent variables - e.g. a dispersion (scale) latent variable -
-		as well as information on how many additional distributional variables are required for this distribution
-		"""
-		self.scale = False
-		self.dof = False
-		self.additional_variables = 0
+    def __init__(self):
+        """
+        Contains information on whether the family has certain kinds of latent variables - e.g. a dispersion (scale) latent variable -
+        as well as information on how many additional distributional variables are required for this distribution
+        """
+        self.scale = False
+        self.dof = False
+        self.additional_variables = 0
 
-	@staticmethod
-	def neg_loglikelihood(Y, theta, scale, dof):
-		"""
-		Creates the negative loglikelihood for the model
+    @staticmethod
+    def neg_loglikelihood(Y, theta, scale, dof):
+        """
+        Creates the negative loglikelihood for the model
 
-		Parameters
+        Parameters
         -------
         Y - tf.placeholder
-        	The data for the target variable
+            The data for the target variable
 
         theta - tf.placeholder
-        	the location parameter for the distribution
+            the location parameter for the distribution
 
         scale - tf.placeholder
-        	the dispersion parameter for the distribution
+            the dispersion parameter for the distribution
 
         dof - tf.placeholder
-        	the degrees of freedom parameter for the distribution
+            the degrees of freedom parameter for the distribution
 
-		Returns
+        Returns
         -------
         - An tf.placeholder with the negative loglikelihood 
-		"""
+        """
 
-		return tf.reduce_mean(-tf.reduce_sum(Y*theta - tf.exp(theta)))
+        return tf.reduce_mean(-tf.reduce_sum(Y*theta - tf.exp(theta)))
 
 
 class StudentT(object):
 
-	def __init__(self):
-		"""
-		Contains information on whether the family has certain kinds of latent variables - e.g. a dispersion (scale) latent variable -
-		as well as information on how many additional distributional variables are required for this distribution
-		"""
-		self.scale = True
-		self.dof = True
-		self.additional_variables = 2
+    def __init__(self):
+        """
+        Contains information on whether the family has certain kinds of latent variables - e.g. a dispersion (scale) latent variable -
+        as well as information on how many additional distributional variables are required for this distribution
+        """
+        self.scale = True
+        self.dof = True
+        self.additional_variables = 2
 
-	@staticmethod
-	def neg_loglikelihood(Y, theta, scale, dof):
-		"""
-		Creates the negative loglikelihood for the model
+    @staticmethod
+    def neg_loglikelihood(Y, theta, scale, dof):
+        """
+        Creates the negative loglikelihood for the model
 
-		Parameters
+        Parameters
         -------
         Y - tf.placeholder
-        	The data for the target variable
+            The data for the target variable
 
         theta - tf.placeholder
-        	the location parameter for the distribution
+            the location parameter for the distribution
 
         scale - tf.placeholder
-        	the dispersion parameter for the distribution
+            the dispersion parameter for the distribution
 
         dof - tf.placeholder
-        	the degrees of freedom parameter for the distribution
+            the degrees of freedom parameter for the distribution
 
-		Returns
+        Returns
         -------
         - An tf.placeholder with the negative loglikelihood 
-		"""
-		return -tf.contrib.distributions.StudentT(dof, theta, scale).log_likelihood(Y)
+        """
+        return -tf.contrib.distributions.StudentT(dof, theta, scale).log_likelihood(Y)
 
 
